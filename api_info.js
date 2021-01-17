3f42d0c4076f7fbaeff80fc2329a72f20a246f70
github username <github email address>
2021-01-17 18:54:23 +0000

    components and containers created

--- api_info.js
+++ api_info.js
@@ -1,1 +1,5 @@
-const Api_key = "this is a super secret password" 
+const finnhub = require('finnhub');
+ 
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = "c0244h748v6vllnqnv6g"
+const finnhubClient = new finnhub.DefaultApi()
