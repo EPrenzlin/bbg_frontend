1fae4371e0a170ff388e97e76e199eb69872939d
github username <eprenzlin@gmail.com>
2021-01-30 14:12:16 +0000

    delete api_info file

--- api_info.js
+++
@@ -1,7 +0,0 @@
-// const finnhub = require('finnhub');
- 
-// const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-// api_key.apiKey = "c0244h748v6vllnqnv6g"
-// const finnhubClient = new finnhub.DefaultApi()
-
-export const token = "meowmeow"
