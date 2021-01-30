cb44c93756465438903cb033f9d40e8645d93e1c
github username <github email address>
2021-01-30 12:31:53 +0000

    removing IPO action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,17 +1,3 @@
 const API = ""
 
-//   export const searchIPO = () => {
-//     return dispatch => {
-//       fetch(API + "/exemptions")
-//       .then(response => response.json())
-//       .then(exemptions => {
-//           dispatch({type:'SET_EXEMPTIONS', exemptions})
-//       })
-//       }
-//     }
-
-    export const searchIPO = () => {
-        finnhubClient.ipoCalendar("2020-01-01", "2020-06-15", (error, data, response) => {
-            console.log(data)
-        })
-    }
+// arrow functions for each action, using the Finhub API JS open library. 
