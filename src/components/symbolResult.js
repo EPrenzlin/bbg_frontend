f05cd416d52915ab3682445955d9149f57d18a27
github username <eprenzlin@gmail.com>
2021-02-06 13:28:44 +0000

    able to display returned fetch api and display

--- src/components/symbolResult.js
+++ src/components/symbolResult.js
@@ -7,9 +7,9 @@ export default class SymbolResult extends Component {
       return( 
     <div> 
 <Card style={{ width: '18rem' }}>   
-<h2> Description:</h2>
-<h2> Symbol: </h2>
-<h2> Type: </h2>
+<h2> Description:{this.props.data.description}</h2>
+<h2> Symbol: {this.props.data.symbol}</h2>
+<h2> Type: {this.props.data.type}</h2>
 </Card>
     </div>
       )
