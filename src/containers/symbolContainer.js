c13e1c63b6da6ca25b1416637eb4b0e2edcfc16c
github username <eprenzlin@gmail.com>
2021-02-07 11:10:22 +0000

    new News components and container created

--- src/containers/symbolContainer.js
+++ src/containers/symbolContainer.js
@@ -2,13 +2,14 @@ import React, {Component} from 'react'
 import SymbolForm from '../components/symbolForm'
 import SymbolResult from '../components/symbolResult'
 import Row from 'react-bootstrap/Row'
+import { Router } from 'react-router-dom'
 
 
 
 export default class SymbolContainer extends Component {
     render() {
     return(
-      <div> 
+      <div>
       <SymbolForm searchSymbol={this.props.searchSymbol}/>
     
       <Row md={3}> 
@@ -17,6 +18,7 @@ export default class SymbolContainer extends Component {
       })
       }
       </Row>
+
     </div> 
     ) 
   } 
