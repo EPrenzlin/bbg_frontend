aaaa0b093f3512ee9a860b160e419401be34f3b8
github username <eprenzlin@gmail.com>
2021-02-21 15:33:38 +0000

    able to display images

--- src/components/newscomponent.js
+++ src/components/newscomponent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react' 
 import Card from 'react-bootstrap/Card'
+import Image from 'react-bootstrap/Image' 
 
 export default class NewsComponent extends Component {
 
@@ -10,8 +11,9 @@ export default class NewsComponent extends Component {
     <Card bg={'dark'} text={'light'} style={{width:'20 rem'}} border={'warning'} >
     <Card.Title>{this.props.data.headline} </Card.Title>
     <Card.Subtitle className="mb-4 text-muted">{this.props.data.category.toUpperCase()} </Card.Subtitle>
-    <h3> {this.props.data.source} </h3>
-    <text> {this.props.data.summary} </text> <br/> 
+    <Image src={this.props.data.image} rounded width="50" height="50"/>
+    {/* <h3> {this.props.data.source} </h3> */}
+    <Card.Text> {this.props.data.summary} </Card.Text> <br/> 
     <Card.Link href={this.props.data.url}>Link to Article</Card.Link>
     </Card>
     </div> 
