10dde8d96ebf7290f44041cf979e764df5861046
github username <eprenzlin@gmail.com>
2021-02-24 20:57:17 +0000

    search profile result amended

--- src/components/profileResult.js
+++ src/components/profileResult.js
@@ -6,17 +6,19 @@ export default class Profileresult extends Component {
     render() {
       return( 
     <div> 
-    <Card> 
-      <h1> Company name: {this.props.results.name} </h1>
-      <h2> Country: {this.props.results.country} </h2>
-      <h2> Currency: {this.props.results.currency}</h2>
-      <h2> IPO date: {this.props.results.ipo}</h2>
-      <h2> Country: {this.props.results.country}</h2>
-      <h2> Industry: {this.props.results.finnhubIndustry}</h2>
-      <h2> Company website: {this.props.results.weburl}</h2>
-      <h2> Primary Exchange: {this.props.results.exchange}</h2>
-      <h2> Total Market Capitalisation: {this.props.results.marketCapitalization}</h2>
-      <h2> Company number: {this.props.results.phone}</h2>
+    <Card>
+      <Card.Text> 
+       Company name: {this.props.results.name} <br/>
+       Country: {this.props.results.country}  <br/>
+       Currency: {this.props.results.currency}  <br/>
+       IPO date: {this.props.results.ipo} <br/>
+       Country: {this.props.results.country}  <br/>
+       Industry: {this.props.results.finnhubIndustry} <br/> 
+       Company website: {this.props.results.weburl} <br/>
+       Primary Exchange: {this.props.results.exchange}  <br/>
+       Total Market Capitalisation: {this.props.results.marketCapitalization} <br/>
+       Company number: {this.props.results.phone} 
+    </Card.Text> 
     </Card>
     </div>
       )
