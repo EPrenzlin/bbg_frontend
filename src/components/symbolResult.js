080e3fac1fdff52989cb86dfd2ac3a93e5ccab94
github username <eprenzlin@gmail.com>
2021-02-24 20:47:12 +0000

    Search bars layout amended

--- src/components/symbolResult.js
+++ src/components/symbolResult.js
@@ -6,10 +6,12 @@ export default class SymbolResult extends Component {
     render() {
       return( 
     <div> 
-<Card>   
-<h2> Description:{this.props.data.description}</h2>
-<h2> Symbol: {this.props.data.symbol}</h2>
-<h2> Type: {this.props.data.type}</h2>
+<Card border="primary">   
+<Card.Text> 
+Description:{this.props.data.description} <br/>
+Symbol: {this.props.data.symbol}<br/>
+Type: {this.props.data.type}<br/>
+</Card.Text>
 </Card>
     </div>
       )
--- src/containers/symbolContainer.js
+++ src/containers/symbolContainer.js
@@ -4,19 +4,21 @@ import SymbolResult from '../components/symbolResult'
 import Row from 'react-bootstrap/Row'
 import { Router } from 'react-router-dom'
 
-
+import Container from 'react-bootstrap/Container'
 
 export default class SymbolContainer extends Component {
     render() {
     return(
       <div>
       <SymbolForm searchSymbol={this.props.searchSymbol}/>
+      <Container>
       <Row md={3}> 
       {this.props.searchResults.map(result => {
         return <SymbolResult data ={result}/>
       })
       }
       </Row>
+      </Container>
 
     </div> 
     ) 
